bd31055689ff18a64f06d690e7f648f9168e31ae
Your Name <you@example.com>
2023-11-30 17:33:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import AppCard from './components/AppCard.vue'
+import axios from 'axios';
+
+export default {
+  components: {
+    AppCard,
+
+  },
+  data() {
+
+    return {
+      beers: ""
+    }
+  },
+
+  mounted() {
+    this.getBeer();
+  },
+
+  methods: {
+    getBeer() {
+      axios.get('https://api.openbrewerydb.org/v1/breweries?by_country=poland&per_page=10').then(risultato => {
+        this.beers = risultato.data
+      })
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <main>
+
+    <AppCard :arrayGenerato="beers" />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+    props: ["arrayGenerato"]
+
+}
+</script>
+
+<template>
+    <div v-for="elemento in arrayGenerato">
+        <span>{{ elemento.name }}</span>
+        <span> {{ elemento.city }}</span>
+    </div>
+</template>
+
+<style scoped></style>
